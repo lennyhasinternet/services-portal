14822601f85cf0d62bd1c5db2d2449799ffe6523
webbandz <tuaman027@aol.com>
2021-02-24 22:04:19 -0500

    First Commit.  Set up properties, dependencies, and database.

---
+++ src/main/java/com/perscholas/merchant/LennyMaseuliMerchantAppCaseStudyApplication.java
@@ -0,0 +1,13 @@
+package com.perscholas.merchant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LennyMaseuliMerchantAppCaseStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LennyMaseuliMerchantAppCaseStudyApplication.class, args);
+	}
+
+}
